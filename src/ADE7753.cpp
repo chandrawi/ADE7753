ae7e500cb5158673db9a98b962f2ab5d273973ef
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2023-02-17 13:13:22 +0700

    Add constructors, begin, end, and SPI transfer methods

--- src/ADE7753.cpp
+++ src/ADE7753.cpp
@@ -1,1 +1,46 @@
 #include <ADE7753.h>
+
+ADE7753::ADE7753(SPIClass &Spi, uint8_t nss, uint32_t spiFreq)
+{
+    _spi = &Spi;
+    _nss = nss;
+    _spiFreq = spiFreq;
+}
+
+ADE7753::ADE7753(uint8_t nss, uint32_t spiFreq)
+{
+    _spi = &SPI;
+    _nss = nss;
+    _spiFreq = spiFreq;
+}
+
+ADE7753::ADE7753()
+{
+    _spi = &SPI;
+    _nss = SS;
+    _spiFreq = ADE7753_SPI_FREQ;
+}
+
+void ADE7753::begin()
+{
+    pinMode(_nss, OUTPUT);
+    _spi->begin();
+}
+
+void ADE7753::end()
+{
+    _spi->end();
+}
+
+void ADE7753::_transfer(uint8_t address, uint8_t* data, uint8_t length)
+{
+    digitalWrite(_nss, LOW);
+    _spi->beginTransaction(SPISettings(_spiFreq, MSBFIRST, SPI_MODE2));
+    _spi->transfer(address);
+    delayMicroseconds(ADE7753_TRA_DEL);
+    for (int8_t i=0; i<length; i++) {
+        data[i] = _spi->transfer(data[i]);
+    }
+    _spi->endTransaction();
+    digitalWrite(_nss, HIGH);
+}
--- src/ADE7753.h
+++ src/ADE7753.h
@@ -4,9 +4,27 @@
 #include <Arduino.h>
 #include <SPI.h>
 
+#define ADE7753_SPI_FREQ    F_CPU / 4
+#define ADE7753_TRA_DEL     4
+
 class ADE7753
 {
+public:
 
+    ADE7753(SPIClass &Spi, uint8_t nss, uint32_t spiFreq = ADE7753_SPI_FREQ);
+    ADE7753(uint8_t nss, uint32_t spiFreq = ADE7753_SPI_FREQ);
+    ADE7753();
+
+    void begin();
+    void end();
+
+private:
+
+    SPIClass* _spi;
+    uint8_t _nss;
+    uint32_t _spiFreq;
+
+    void _transfer(uint8_t address, uint8_t* data, uint8_t length);
 };
 
 #endif
